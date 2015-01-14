7eabeab647982ed98f7cb0eac08a62576fb70247
Artyom Petrenkov <static.lab@gmail.com>
2015-01-14 22:46:40 +0300

    feat(test): issue #9

--- issue #9/static/js/main.js
+++ issue #9/static/js/main.js
@@ -1,10 +1,16 @@
 $(function() {
-	var format = function(state) {
-			if (!state.id)  {
-				return state.text;
-			}
-			return '<i class="b-info">link</i>' + state.text;
+	var format = function(state, container) {
+        if (!state.id) {
+			return state.text;
 		}
+        container.append(document.createTextNode(state.text));
+        container.prepend(
+            $('<i class="b-info">link</i>').mousedown(function(event) {
+                event.preventDefault();
+                alert(state.text);
+            })
+        );
+	};
 
 	$('.b-select').select2({
 		formatResult: format,
